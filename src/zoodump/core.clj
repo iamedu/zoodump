96650599283d904cb665ecc35361cbc58e4f798e
Eduardo Diaz <iamedu@Gmail.com>
2013-09-16 10:36:45 -0500

    Adding cli

--- src/zoodump/core.clj
+++ src/zoodump/core.clj
@@ -1,7 +1,15 @@
 (ns zoodump.core
+  (:require [clojure.tools.cli :refer [cli]])
   (:gen-class))
+
+(defn process-request [opts]
+  (println opts))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [[options args banner] (cli args
+                                  ["-h" "--help" "Show help" :default false :flag true])]
+    (if (:help options)
+      (println banner)
+      (process-request options))))
